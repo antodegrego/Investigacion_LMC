b099cab95ad28432411c83231b367216fbea0e3f
antodegrego <adegregoriocortes@gmail.com>
2026-06-23 11:08:07 -0400

    ahora construimos cprops para 13CO con su mascara

--- conversion_tabla_cprops.py
+++ conversion_tabla_cprops.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
 
-archivo = r"C:\Users\adegr\OneDrive\Escritorio\Bibliografía Magister\Investigacion_LMC\12CO21_mask13CO.props.fits"
+archivo = r"C:\Users\adegr\OneDrive\Escritorio\Bibliografía Magister\Investigacion_LMC\12CO21_mask13CO_corrected.props.fits"
 hdul = fits.open(archivo)
 hdul.info()
 
@@ -8,4 +8,4 @@ from astropy.table import Table
 
 t = Table.read(archivo)
 
-t.write("NMoIR_12CO21_mask13CO.props.csv", format="csv", overwrite=True)
+t.write("NMoIR_12CO21_mask13CO_corrected.props.csv", format="csv", overwrite=True)
--- cprops_CO.py
+++ cprops_CO.py
@@ -6,7 +6,7 @@ from spectral_cube import SpectralCube
 # Permitir operaciones grandes
 #SpectralCube.allow_huge_operations = True
 
-cubefile = 'NMolR_aca_12CO21_K_cube.fits' #Your cube
+cubefile = 'NMolR_aca_13CO21_K_cube.fits' #Your cube
 mask = 'NMolR_aca_13CO21_mask_clean.fits' # Mask defining where to find emission
 d = 50.0 * u.kpc           # Distance (with units)
 
@@ -15,5 +15,5 @@ pycprops.fits2props(cubefile,
                     mask_file=mask,
                     distance=d, 
                     noise_file='noise_cube13.fits',
-                    asgnname='12CO21_mask13CO_corrected.asgn.fits',
-                    propsname='12CO21_mask13CO_corrected.props.fits')
+                    asgnname='13CO21_mask13CO_corrected.asgn.fits',
+                    propsname='13CO21_mask13CO_corrected.props.fits')
