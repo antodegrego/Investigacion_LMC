effe304d71812e7601d2e34290f9814d33e5debc
antodegrego <adegregoriocortes@gmail.com>
2026-06-22 22:22:36 -0400

    Cambio mascara cprops

--- cprops_CO.py
+++ cprops_CO.py
@@ -5,13 +5,13 @@ from spectral_cube import SpectralCube
 
 
 cubefile = 'NMolR_aca_12CO21_K_cube.fits' #Your cube
-mask = 'NMolR_aca_mosaic_12CO21_contsub_masked.fits' # Mask defining where to find emission
+mask = 'NMolR_aca_13CO21_mask_3sigma.fits' # Mask defining where to find emission
 d = 50.0 * u.kpc           # Distance (with units)
 
 
 pycprops.fits2props(cubefile,
                     mask_file=mask,
                     distance=d, 
-                    noise_file='noise_cube12.fits',
-                    asgnname='12CO21.asgn.fits',
-                    propsname='12CO21.props.fits')
+                    #noise_file='noise_cube12.fits',
+                    asgnname='12CO21_mask13CO.asgn.fits',
+                    propsname='12CO21_mask13CO.props.fits')
